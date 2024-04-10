d33f321744486caac2ec37ce87ccc14271df98f8
Aleks Gomeniuk <agsfinks@gmail.com>
2024-04-10 15:56:56 +0100

    initial commit

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+function App() {
+	return (
+		<>
+			<h1>Hi Milla!</h1>
+			<ol>
+				<li>Research</li>
+				<li>Flow chart</li>
+				<li>Design sketch</li>
+				<li>Implementation</li>
+			</ol>
+		</>
+	);
+}
+
+export default App;
